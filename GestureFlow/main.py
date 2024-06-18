89e039fbf1a7c29ca7a6111794b1c3d046e7871f
Lucas Lucio <lukasduluciocm@gmail.com>
2024-06-18 01:47:52 -0300

    alterado e descrito o código

--- GestureFlow/main.py
+++ GestureFlow/main.py
@@ -1,85 +1,72 @@
+#Importações das bibliotecas
 import numpy as np
 import cv2
-import os
 from cvzone.HandTrackingModule import HandDetector
-import keyboard
+from pynput.keyboard import Key, Controller
 
-# Parameters
-width, height = 1280, 720
-FolderPath = 'ppt'                  # Directory in which Presentation Slides are kept
-hs, ws = 240, 420
+# Cria uma instância do controlador de teclado.
+keyboard = Controller()
 
-# Cam_Setup
-
+# Define a largura e altura da imagem capturada, exibição, e iniciliza a captura
+width, height = 1280, 720 
+hs, ws = 240, 420 
 cam = cv2.VideoCapture(0)
 cam.set(3, ws)
 cam.set(4, hs)
 
-# Presentation_Slides -> Getting the list of the slide names and sorting them according to their length [ length of the slide name ]
-# sorted_fuction
-pathSlides = sorted(os.listdir(FolderPath), key=len)
-# print(pathSlides)
-
-# Variables
-SlideNum = 0
+# Define um limite para a detecção de gestos.
 gestureThreshold = 300
-button_pressed = False
-button_counter = 0
-button_delay = 30
-annotations = [[]]
-annotation_number = -1
-annotation_start = False
 
-# Hand_Detector
-# Here, detectionCon = the code will run if it is 80% of the oblect being a hand.
+ # Inicializa o detector de mãos com uma confiança de detecção de 0.8 e detectando no máximo uma mão.
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
+#Variável de controle para troca de slides
+slideBlock = False
+
+#Loop para processamento da imagem capturada em tempo real
 while True:
+    #Captura quadro, corrigem orientação da imagem, e verifica limite 
     success, img = cam.read()
-    # To flip the img 1= horizontal , 0= vertical ; but here right becomes left and vice versa.
     img = cv2.flip(img, 1)
     img = cv2.line(img, (0, gestureThreshold), (width, gestureThreshold), (100, 255, 205), 10)
 
-    # Here FolderPath is the Project Directory
-    pathFullSlides = os.path.join(FolderPath, pathSlides[SlideNum])
-    SlideCurrent = cv2.imread(pathFullSlides)
 
-# cv2.flip( var, 1) + flipType = False == Perfectly flipped img.
+    #Detecta as mãos na imagem
     hands, img = detector.findHands(img, flipType=False)  # flipType = Flase it will nt flip the img. right will remain right and vice versa.
 
-    if hands and button_pressed is False:
+    #Processa para caso encontrou uma mão
+    if hands:
+
+        #Seleciona a primeira mão, obtem estado dos dedos, obtem dados da localização da mão
         hand = hands[0]
         fingers = detector.fingersUp(hand)
         cx, cy = hand['center']
-        #land_mark_list
         lm_list = hand['lmList']
 
+        #Verifica se mão está no limite para detecção
+        if cy <= gestureThreshold:
 
-        # Constrain Region for pointer
-        #np.interp(variable[fitting-size],[actual_size])
-        x_val = int(np.interp(lm_list[8][0], [0, width//4], [0, width]))
-        y_val = int(np.interp(lm_list[8][1], [0, height-500], [0, height]))
-        index_finger = x_val, y_val
+            #Verifica gesto slide anterior
+            if fingers == [1, 1, 0, 0, 0] and slideBlock == True:
+                keyboard.press(Key.down) #Pressiona tecla
+                keyboard.release(Key.down) #Libera tecla
+                slideBlock = False #Gerencia controle de slides
+            
 
-        if cy <= gestureThreshold:  #If hand is above the gestureThreshold
-            print(fingers)
-            #Gesture-0 Left
+            #Verifica gesto próximo slide
+            if fingers == [0, 0, 0, 0, 0] and slideBlock == True:
+                keyboard.press(Key.up) #Pressiona tecla
+                keyboard.release(Key.up) #Libera tecla
+                slideBlock = False #Gerencia controle de slides
+
+            #Verifica gesto para liberação de bloqueio dos slides
             if fingers == [1, 0, 0, 0, 0]:
-                keyboard.send(hotkey='Left',do_press=True,do_release=True)
-
-            # Gesture-1 Right
-            if fingers == [1, 0, 0, 0, 1]:
-                keyboard.send(hotkey='Right',do_press=True,do_release=True)
-
-
-    # Adding WebCam Img to the Slide
-    imgSmall = cv2.resize(img, (ws, hs))
-    h, w, _ = SlideCurrent.shape
-    SlideCurrent[0:hs, w - ws:w] = imgSmall
-
-    cv2.imshow('Slides', SlideCurrent)
+                slideBlock = True #Gerencia controle de slides
+    
+    #Exibe imagem processada
     cv2.imshow('Image', img)
 
-    Key = cv2.waitKey(1)
-    if Key == ord('q'):
+    #Aguarda chamada para encerramento
+    KeyCV = cv2.waitKey(1)
+    if KeyCV == ord('s'):
         break
